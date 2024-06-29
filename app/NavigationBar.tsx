97b0bbe49ba678e0ccb2a573724e23f7708580bd
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-06-30 02:05:13 +0300

    Refactor Navbar to have links to other pages

--- app/NavigationBar.tsx
+++ app/NavigationBar.tsx
@@ -15,18 +15,16 @@ const NavigationBar = () => {
         <p className="font-bold text-inherit text-xl">Blog</p>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <Input
-          classNames={{
-            base: "max-w-full sm:max-w-[20rem] h-10",
-            mainWrapper: "h-full",
-            input: "text-small",
-            inputWrapper:
-              "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
-          }}
-          placeholder="Type to search..."
-          size="sm"
-          type="search"
-        />
+        <NavbarItem isActive>
+          <Link color="foreground" href="/">
+            Home
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="/posts">
+            Posts
+          </Link>
+        </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem className="hidden lg:flex">
