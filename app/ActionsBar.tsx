09d001d8cf2d7140ef1de79deb99a0a63cf2f593
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-06-30 02:05:23 +0300

    Add the search field to the actions

--- app/ActionsBar.tsx
+++ app/ActionsBar.tsx
@@ -1,11 +1,20 @@
-import { Button } from "@nextui-org/react";
+import { Button, Input } from "@nextui-org/react";
 
 const ActionsBar = () => {
   return (
     <div className="flex justify-between ">
-      <Button className="text-md text-white" color="success" radius="none">
-        Create new
-      </Button>
+      <Input
+        classNames={{
+          base: "max-w-full sm:max-w-[20rem] h-10",
+          mainWrapper: "h-full",
+          input: "text-small",
+          inputWrapper:
+            "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+        }}
+        placeholder="Type to search..."
+        size="sm"
+        type="search"
+      />
       <Button className="text-md text-white" color="success" radius="none">
         Create new
       </Button>
