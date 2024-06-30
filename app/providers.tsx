126a1dbfb5ed89c9ce9420b5942c2c7cab8866c6
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-07-01 01:00:05 +0300

    Move Container to ui

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
-import Container from "./Container";
+import Container from "./ui/Container";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
