086bcfcf585af82b5b2eae2d3b0160f22da427bc
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-06-30 18:22:40 +0300

    Dynamically import the creation form

--- app/posts/create/page.tsx
+++ app/posts/create/page.tsx
@@ -1,17 +1,14 @@
-import { Button, Input, Textarea } from "@nextui-org/react";
-import { createPost } from "../actions";
-import "easymde/dist/easymde.min.css";
-import SimpleMDE from "react-simplemde-editor";
+import dynamic from "next/dynamic";
+
+const Form = dynamic(() => import("./NewPostFields"), {
+  ssr: false,
+});
 
 const Page = () => {
   return (
-    <form action={createPost} className="flex flex-col gap-3 mt-4">
-      <Input placeholder="Title" name="title" />
-      <Textarea placeholder="Content" name="content" />
-      <Button type="submit" color="success">
-        Create
-      </Button>
-    </form>
+    <div>
+      <Form />
+    </div>
   );
 };
 
