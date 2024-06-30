f34e7b31f373e8d19fec1b24c9fc55390b30288a
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-07-01 02:17:24 +0300

    Delete a post

--- app/posts/actions.ts
+++ app/posts/actions.ts
@@ -1,1 +1,19 @@
 "use server";
+
+import prisma from "@/prisma/prismaClient";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export const deletePost = async (formData: FormData) => {
+  const id = formData.get("id") as string;
+  if (id) {
+    await prisma.post.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    revalidatePath("/posts");
+    redirect("/posts");
+  }
+};
