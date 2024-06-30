399b33da78234130ae6234f8f7f2deb86df23f37
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-06-30 04:55:29 +0300

    Remove caching

--- app/utils/getPosts.ts
+++ app/utils/getPosts.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { cache } from "react";
 
 const prisma = new PrismaClient();
 
-export const getPosts = cache(async () => await prisma.post.findMany());
+export const getPosts = async () => await prisma.post.findMany();
