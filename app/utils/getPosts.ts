ab244bd9ae4ee8fbcc53dea16c69337cc2d977c1
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-06-30 17:27:09 +0300

    Order posts by creation date

--- app/utils/getPosts.ts
+++ app/utils/getPosts.ts
@@ -2,4 +2,9 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const getPosts = async () => await prisma.post.findMany();
+export const getPosts = async () =>
+  await prisma.post.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
