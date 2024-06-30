89a16a89c07903ccb3a2676cb908ca9c7c3bb96b
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-06-30 19:06:14 +0300

    Add Link to the edit page

--- app/posts/PostsList.tsx
+++ app/posts/PostsList.tsx
@@ -1,14 +1,17 @@
 import { Card, CardBody, CardHeader } from "@nextui-org/react";
 import { Post } from "@prisma/client";
+import Link from "next/link";
 
 const PostsList = ({ posts }: { posts: Post[] }) => {
   return (
     <div className="grid grid-cols-2 gap-3 mt-4">
       {posts.map((p) => (
         <Card key={p.id} radius="none">
-          <CardHeader className="prose hover:cursor-pointer">
-            <h2>{p.title}</h2>
-          </CardHeader>
+          <Link href={`/posts/edit/${p.id}`}>
+            <CardHeader className="prose hover:cursor-pointer">
+              <h2>{p.title}</h2>
+            </CardHeader>
+          </Link>
           <CardBody>
             {p.content.length > 100
               ? p.content.substring(0, 100) + "..."
