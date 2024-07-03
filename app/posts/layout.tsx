0eabea915e2cf60238af98e0b41ad63860bcfc55
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-07-03 06:28:46 +0300

    Remove dynamic import

--- app/posts/layout.tsx
+++ app/posts/layout.tsx
@@ -1,7 +1,4 @@
 import { PropsWithChildren } from "react";
-import dynamic from "next/dynamic";
-const dynamicImport = dynamic;
-export { dynamicImport };
 
 const Layout = ({ children }: PropsWithChildren) => {
   return <div>{children}</div>;
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -6,7 +6,7 @@ import PostsSkeleton from "../ui/skeletons/PostsSkeleton";
 import { numberOfPages } from "../utils/getPageSize";
 import Pagination from "./components/Pagination";
 import { getPostsAuthors } from "../utils/getAuthors";
-import { dynamicImport } from "./layout";
+import ActionsAccordion from "./components/ActionsAccordion";
 
 interface PostsSearchParams {
   searchParams: {
@@ -15,11 +15,6 @@ interface PostsSearchParams {
     orderBy: "asc" | "desc";
   };
 }
-
-const ActionsAccordion = dynamicImport(
-  () => import("./components/ActionsAccordion"),
-  { ssr: false }
-);
 
 const Page = async ({ searchParams }: PostsSearchParams) => {
   const userId = parseInt(searchParams.user);
